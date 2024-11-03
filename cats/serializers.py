acd9f261b6be88901bd63c85091963b1b2c426b8
Владислав Наумец <klinicpanik@yandex.ru>
2024-11-03 14:28:14 +0300

    n

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,10 +1,18 @@
+# cats/serializers.py
 from rest_framework import serializers
 
-from .models import Cat
+from .models import Cat, Owner
 
 
 class CatSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year')
+        fields = ('id', 'name', 'color', 'birth_year', 'owner')
+
+
+class OwnerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Owner
+        fields = ('first_name', 'last_name','cats')
